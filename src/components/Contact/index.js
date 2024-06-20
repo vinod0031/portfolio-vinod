a0b28ca4c1da2f21f849165fc57076c5c76bc474
vinod0031 <kvinodkumar0031@gmail.com>
2024-06-20 12:15:30 +0530

    first commit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,6 +8,7 @@ const Contact = () => {
     emails: '',
     message: '',
   });
+  const [loading, setLoading] = useState(false); // State for loading indicator
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -15,6 +16,7 @@ const Contact = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true); // Set loading to true when form is submitted
     try {
       const response = await fetch('https://portfolio-backend-tgdg.onrender.com/send', {
         method: 'POST',
@@ -30,25 +32,32 @@ const Contact = () => {
       }
     } catch (error) {
       alert('Error: ' + error.toString());
+    } finally {
+      setLoading(false); // Reset loading to false when request completes
     }
   };
 
   return (
     <div className='contact-container'>
       <div>
-        <img src={contact} alt="image-error"/>
+        <img src={contact} alt="Contact"/>
       </div>
       <div className='contact-input'>
-      <h1>Contact Me</h1><br></br>
-      <form onSubmit={handleSubmit}>
-        
-        <input className="input-space"  type="text" name="name" value={formData.name} onChange={handleChange} required placeholder='Name'/><br></br>
-        
-        <input className="input-space"  type="email" name="emails" value={formData.emails} onChange={handleChange} required placeholder='Email'/><br></br>
-        
-        <textarea className="input-space"  name="message" value={formData.message} onChange={handleChange} required placeholder='Message'></textarea><br></br>
-        <button className='button-send' type="submit">Send</button>
-      </form></div>
+        <h1>Contact Me</h1><br/>
+        <form onSubmit={handleSubmit}>
+          <input className="input-space"  type="text" name="name" value={formData.name} onChange={handleChange} required placeholder='Name'/><br/>
+          <input className="input-space"  type="email" name="emails" value={formData.emails} onChange={handleChange} required placeholder='Email'/><br/>
+          <textarea className="input-space"  name="message" value={formData.message} onChange={handleChange} required placeholder='Message'></textarea><br/>
+          {loading ? (
+            <button className='button-send' type="button" disabled>
+              <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+              Sending...
+            </button>
+          ) : (
+            <button className='button-send' type="submit">Send</button>
+          )}
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import vinodImage from '../../assets/Images/vinod_new.png';
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 import './index.css'
 const Home = () => {
+    useEffect(()=>{
+        Aos.init();
+    },[])
 return (
     <div className='Home-container'>
 <div>
-    <img className='vinod-image' src={vinodImage} alt='image-error'/>
+    <img  data-aos="fade-right" className='vinod-image' src={vinodImage} alt='image-error'/>
 </div>
 <div className='page'>
-    <h1 className='hi'>Hi,I'm <span className='vinod'>Vinod Kumar,</span></h1><br>
-    </br><h1 className='heading-fulstack'>Full Stack Web Developer !</h1><br></br>
+    <h1 data-aos="fade-right" className='hi'>Hi,I'm <span className='vinod'>Vinod Kumar,</span></h1><br>
+    </br><h1   data-aos="fade-left" className='heading-fulstack'>Full Stack Web Developer !</h1><br></br>
     <br></br>
-    <p className='pera'>I am a passionate and skilled Full-Stack Developer with a solid foundation in both front-end and back-end technologies. With proven experience in developing responsive and user-friendly web applications using React.js, Node.js, and various database systems, I excel in collaborating with cross-functional teams to deliver high-quality software solutions. I am committed to continuous learning and staying updated with the latest industry trends.</p>
+    <p  data-aos="fade-right" className='pera'>I am a passionate and skilled Full-Stack Developer with a solid foundation in both front-end and back-end technologies. With proven experience in developing responsive and user-friendly web applications using React.js, Node.js, and various database systems, I excel in collaborating with cross-functional teams to deliver high-quality software solutions. I am committed to continuous learning and staying updated with the latest industry trends.</p>
     <br></br><a href='https://drive.google.com/file/d/1Dxrme2__c73K_r9O1ek-DqAeODH-WJt3/view?usp=drive_link' target='_blank'><button className="button" >Download Resume</button></a>
 </div>
     
